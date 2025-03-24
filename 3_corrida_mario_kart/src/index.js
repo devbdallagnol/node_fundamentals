dc8b004c669c0ae88dc501f44209c18ab4947a36
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-24 18:49:10 -0300

    added - blocks (SWITCH CASE)

--- 3_corrida_mario_kart/src/index.js
+++ 3_corrida_mario_kart/src/index.js
@@ -18,11 +18,32 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomBlock() {
+  let random = Math.random();
+  let result
+
+  switch (true) {
+    case random < 0.33:
+      result = "⬆️ RETA"
+      break;
+    case random < 0.66:
+      result = "⤴️ CURVA"
+      break;
+    default:
+      result = "💣 CONFRONTO"
+      break;
+  }
+
+  return result;
+}
+
 async function playRaceEngine(character1, character2){
   for(let round = 1; round <= 5; round++){
     console.log(`🏁 Rodada ${round} 🏁`);
 
     // sortear bloco
+    let block = await getRandomBlock();
+    console.log(`Bloco sorteado: ${block}`);
   }
 }
 
