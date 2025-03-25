3251620c212d2e7a7c930329d0cb04dabfe9f68e
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-25 12:53:57 -0300

    added - assinaturas cart.js, item.js

---
+++ 6_shopee_cart/src/services/cart.js
@@ -0,0 +1,12 @@
+// quais ações meu carrinho pode fazer?
+
+// CASOS DE USO
+
+// 1. -> Adicionar um item
+async function addItem(userCart){}
+// 2. -> Deletar o carrinho
+async function deleteItem(userCart, name){}
+// 3. -> Remover um item
+async function removeItem(userCart, index){}
+// 4. -> Calcular o total do carrinho
+async function calculateTotal(userCart){}
