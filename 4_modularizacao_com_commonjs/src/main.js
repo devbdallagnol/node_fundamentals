af143b5147d9a426430172497803d3bd19e9ba75
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-25 10:13:10 -0300

    added - exports const

--- 4_modularizacao_com_commonjs/src/main.js
+++ 4_modularizacao_com_commonjs/src/main.js
@@ -1,10 +1,14 @@
 const p = require("./services/products");
+const config = require("./services/config");
 
 async function main() {
   console.log('🛒 Carrinho de compras 🛒');
-  p.getFullName("408", "mousepad");
-  p.getFullName("508", "mouse");
-  p.getProductLabel("teclado");
+//   p.getFullName("408", "mousepad");
+//   p.getFullName("508", "mouse");
+//   p.getProductLabel("teclado");
+
+console.log(config.devArea);
+console.log(config.client);
 }
 
 main();
