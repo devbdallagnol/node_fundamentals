9d96b093abab8a9520e072daeb68a9fd1b5b0a91
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-25 11:23:38 -0300

    added - destructuring

--- 5_esm_modules/src/index.js
+++ 5_esm_modules/src/index.js
@@ -1,6 +1,6 @@
-import * as database from './utils/database.js';
+import { connectToDatabase } from './utils/database.js';
 
 console.log('Hello, Ecma!');
 // connectToDatabase('MySQL'); // Conectado ao banco MySQL!
 
-database.connectToDatabase('MySQL'); // Conectado ao banco MySQL!
+connectToDatabase('MySQL'); // Conectado ao banco MySQL!
