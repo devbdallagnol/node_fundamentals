b80afccc58b95eb47cbba38df694cd7a6fed0b68
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-25 18:06:04 -0300

    added - dev dependencies -D

--- 7_npm_packages_basic/src/index.js
+++ 7_npm_packages_basic/src/index.js
@@ -1,4 +1,6 @@
 import chalk from 'chalk';
+import logSymbols from 'log-symbols';
 
-console.log(chalk.blue.bgGrey.italic('Ola, eu sou'));
-console.log(chalk.red.bgYellow.bold('Jarvis'));
+console.log(logSymbols.success, chalk.green.italic('Servidor iniciado ...!'));
+
+console.log(logSymbols.error, chalk.red.italic('Servidor com problema ...!'));
