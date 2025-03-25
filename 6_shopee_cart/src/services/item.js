6cf0c5dc63ee7046591872acaeb05145615c974b
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-25 13:06:55 -0300

    added - implementando a logica do item.js

--- 6_shopee_cart/src/services/item.js
+++ 6_shopee_cart/src/services/item.js
@@ -1,6 +1,13 @@
 // CASOS DE USO DOS ITEMS
 
 // 1. -> Criar um item com subtotal certo
-async function createItem(name, price, quantity){}
+async function createItem(name, price, quantity){
+    return {
+        name,
+        price,
+        quantity,
+        subtotal: () => price * quantity
+    };
+}
 
-
+export default createItem;
