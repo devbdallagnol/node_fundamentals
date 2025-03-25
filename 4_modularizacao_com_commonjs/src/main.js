c41ab3bfb3cc33c913757ee761ab78d56387bfcc
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-25 10:34:26 -0300

    added - destructuring

--- 4_modularizacao_com_commonjs/src/main.js
+++ 4_modularizacao_com_commonjs/src/main.js
@@ -1,22 +1,16 @@
-const product = require("./services/products");
+const { getFullName, productType } = require("./services/products");
+const products = require("./services/products");
+
 const config = require("./services/config");
 const database = require("./services/database");
 
 async function main() {
   console.log("🛒 Carrinho de compras 🛒");
-  //   p.getFullName("408", "mousepad");
-  //   p.getFullName("508", "mouse");
-  //   p.getProductLabel("teclado");
 
-  console.log(product.productType);
-  console.log(config.devArea);
-  console.log(config.client);
-  product.getFullName("408", "mousepad");
-  product.getFullName("508", "mouse");
-  product.getProductLabel("teclado");
+  getFullName("408", "mousepad");
+  products.getFullName("508", "mouse");
 
   database.connectToDatabase("Products");
-  database.disconnectDatabase();
 }
 
 main();
