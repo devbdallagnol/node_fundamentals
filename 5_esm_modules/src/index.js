aae5cf711f64bda07a0afdc29f482d55ed83c131
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-25 11:33:18 -0300

    added - direct exports

--- 5_esm_modules/src/index.js
+++ 5_esm_modules/src/index.js
@@ -1,9 +1,9 @@
 import { connectToDatabase, databaseType } from './utils/database.js';
+import { getDataFromApi } from './utils/api.js';
+import * as api from './utils/api.js';
 
 console.log('Hello, Ecma!');
 // connectToDatabase('MySQL'); // Conectado ao banco MySQL!
 
-connectToDatabase('MySQL'); // Conectado ao banco MySQL!
-
-databaseType.userType = 'user';
-console.log(databaseType.userType); // user
+getDataFromApi(); // Dados da API sendo buscados ...!
+api.getDataFromApi(); // Dados da API sendo buscados ...!
---
+++ 5_esm_modules/src/utils/api.js
@@ -0,0 +1,8 @@
+export const key = {
+    value: "2313jklfds",
+    permission: "admin",
+};
+
+export const getDataFromApi = () => {
+    console.log("Dados da API sendo buscados ...!");
+}
