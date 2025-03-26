4346d30d252862c6eff270f1642dc8cd5dbd10ea
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-26 11:48:57 -0300

    added - dotenv as package

--- 9_npm_enviroment/src/index.js
+++ 9_npm_enviroment/src/index.js
@@ -1,4 +1,5 @@
 import connectToDatabase from "./database/data.js";
+import 'dotenv/config';
 
 async function main(){
     await connectToDatabase(process.env.USERDATABASE, process.env.PASSWORDDATABASE);
