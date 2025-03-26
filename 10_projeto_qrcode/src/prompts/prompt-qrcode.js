1344d09f04cd481baf00f4604f4022a90be3e33a
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-26 13:34:28 -0300

    added - Qr-Code Generate

--- 10_projeto_qrcode/src/prompts/prompt-qrcode.js
+++ 10_projeto_qrcode/src/prompts/prompt-qrcode.js
@@ -1,5 +1,17 @@
 import chalk from "chalk";
 
-const promptQRCode = [];
+const promptQRCode = [
+    {
+        name: "link",
+        description: chalk.yellow("Digite o link para gerar o QR Code: "),
+    },
+    {
+        name: "type",
+        description: chalk.yellow("Escolha entre o tipo (1- NORMAL | 2- TERMINAL): "),
+        pattern: /^[1-2]+$/,
+        message: chalk.red("Escolha apenas entre 1 e 2: "),
+        required: true
+    }
+];
 
 export default promptQRCode;
