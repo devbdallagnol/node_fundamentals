0d020e6eb2c78f246e510ebd7f0fe35565e27647
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-26 13:53:33 -0300

    added - Password create

--- 10_projeto_qrcode/src/services/password/create.js
+++ 10_projeto_qrcode/src/services/password/create.js
@@ -2,6 +2,8 @@ import chalk from "chalk";
 
 async function createPassword(password) {
   console.log(chalk.green("Password created successfully"));
+  const password = await handle();
+  console.log(chalk.green("Password: " + password));
 }
 
 export default createPassword;
