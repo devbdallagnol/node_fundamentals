c3eb894681bfe2565b19935a5fcce3e05436702d
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-03-27 12:22:42 -0300

    added - npm trends, tsup | npm i tsup -D

---
+++ 11_Typescript/dist/index.cjs
@@ -0,0 +1,8 @@
+"use strict";
+
+// src/index.ts
+function main() {
+  let myEmail = "ola brun\xE3o";
+  console.log(myEmail);
+}
+main();
--- 11_Typescript/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function main() {
-    let myEmail = "brunao@email.com";
-    console.log(myEmail);
-}
-main();
