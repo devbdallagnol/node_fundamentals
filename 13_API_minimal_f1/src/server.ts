6d1235d7bb69873d102ca87c02ce5b4dfecd299d
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-09 12:26:55 -0300

    added - filtrando pilotos por id

--- 13_API_minimal_f1/src/server.ts
+++ 13_API_minimal_f1/src/server.ts
@@ -1,31 +1,49 @@
 import fastify from "fastify";
 
-const server = fastify({logger: true});
+const server = fastify({ logger: true });
 
 const teams = [
-    {id: 1, name: "McLaren", base: "Woking, United Kingdom"},
-    {id: 2, name: "Mercedes", base: "Brackley, United Kingdom"},
-    {id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom"},
-]
+  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+];
 
 const drivers = [
-    {id: 1, name: "Max Verstappen", team: "Red Bull Racing"},
-    {id: 2, name: "Lewis Hamilton", team: "Ferrari"},
-    {id: 2, name: "Lando Norris", team: "McLaren"},
-]
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
+  { id: 2, name: "Lando Norris", team: "McLaren" },
+];
 
+server.get("/teams", async (request, response) => {
+  response.type("application/json").code(200);
+  return { teams };
+});
 
+server.get("/drivers", async (request, response) => {
+  response.type("application/json").code(200);
+  return { drivers };
+});
 
-server.get("/teams", async(request, response) => {
-    response.type("application/json").code(200)
-    return { teams };
-})
+interface DriverParams {
+  id: string;
+}
 
-server.get("/drivers", async(request, response) => {
-    response.type("application/json").code(200)
-    return { drivers };
-})
+server.get<{ Params: DriverParams }>(
+  "/drivers/:id",
+  async (request, response) => {
+    const id = parseInt(request.params.id);
+    const driver = drivers.find((d) => d.id === id);
 
-server.listen({port: 3333}, ()=>{
-    console.log("Server init")
-})
+    if (!driver) {
+    response.type("application/json").code(404);
+    return { message: "Driver not found" };
+    }else{
+        response.type("application/json").code(200);
+        return { driver };
+    }
+}
+);
+
+server.listen({ port: 3333 }, () => {
+  console.log("Server init");
+});
