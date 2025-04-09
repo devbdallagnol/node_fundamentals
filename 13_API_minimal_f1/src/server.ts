f4a331580da5e7bee241e428b57ecde734120e0b
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-09 12:06:03 -0300

    added - criando os endpoints teams e drivers

--- 13_API_minimal_f1/src/server.ts
+++ 13_API_minimal_f1/src/server.ts
@@ -14,6 +14,16 @@ server.get("/teams", async(request, response) => {
     ]
 })
 
+server.get("/drivers", async(request, response) => {
+    response.type("application/json").code(200)
+    return [
+        {id: 1, name: "Charles Leclerc", team: "Ferrari"},
+        {id: 2, name: "Carlos Sainz", team: "Ferrari"},
+        {id: 3, name: "Lewis Hamilton", team: "Mercedes"}
+    ]
+})
+
+
 // escutar alguma porta, para o q algum client possa se comunicar com ele
 server.listen({port: 3333}, ()=>{
     console.log("Server init")
