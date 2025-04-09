9f9b5eee52b05654f00238290bb194c2dc9d4768
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-09 12:01:40 -0300

    added - criando uma Fast API

--- 13_API_minimal_f1/src/server.ts
+++ 13_API_minimal_f1/src/server.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({logger: true});
+
+// listar as equipes de f1
+// implentar o verbo http GET
+// preciso passar uma rota para ele
+// preciso criar meu controller
+server.get("/teams", async(request, response) => {
+    response.type("application/json").code(200)
+
+    return [
+        {id: 1, name: "Ferrari"},
+    ]
+})
+
+// escutar alguma porta, para o q algum client possa se comunicar com ele
+server.listen({port: 3333}, ()=>{
+    console.log("Server init")
+})
