e693d6b7d775642ca9908d77df46ecbd08d52ef1
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-09 10:06:49 -0300

    added - criando rotas

--- 12_podcast/src/server.ts
+++ 12_podcast/src/server.ts
@@ -1,17 +1,27 @@
-import * as http from 'http';
-import { getListEpisodes } from './controllers/podcasts-controller';
+import * as http from "http";
+import {
+  getFilterEpisodes,
+  getListEpisodes,
+} from "./controllers/podcasts-controller";
 
-const server = http.createServer( 
-    async (req: http.IncomingMessage, res: http.ServerResponse) => {
-        if (req.method === 'GET'){
-            // chama o controller para listar os episódios
-           await getListEpisodes(req, res);
-        }
+const server = http.createServer(
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    // listar podcasts
+    if (req.method === "GET" && req.url === "/api/list") {
+      // chama o controller para listar os episódios
+      await getListEpisodes(req, res);
     }
+
+    // filtrar podcasts
+    if (req.method === "GET" && req.url === "/api/episode") {
+      // chama o controller para filtrar os episódios
+      await getFilterEpisodes(req, res);
+    }
+  }
 );
 
 const port = process.env.PORT;
 
 server.listen(port, () => {
-    console.log(`Servidor iniciado na porta ${port}`);
-})
+  console.log(`Servidor iniciado na porta ${port}`);
+});
