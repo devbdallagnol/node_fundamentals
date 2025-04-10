7dca53ac41ed4300cdcdcf3914f41211b958f52d
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-10 11:34:36 -0300

    added - criando a camada de Service

--- 14_Champions_league_nodejs_express/src/controllers/players-controller.ts
+++ 14_Champions_league_nodejs_express/src/controllers/players-controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
+import { getPlayerService } from "../services/players-service";
 
-export const getPlayer = (req: Request, res: Response) => {
-  res.status(200).json({ player: "Ronaldo", team: "Real Madrid" });
+export const getPlayer = async (req: Request, res: Response) => {
+  
+  const responseService = await getPlayerService();
+  
+  res.status(200).json(responseService);
 };
