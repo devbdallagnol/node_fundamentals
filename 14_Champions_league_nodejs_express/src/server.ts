213e71ded6be41081752db7e921321aab7bd189c
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-10 10:59:07 -0300

    added - utilizando o express | npm i express

--- 14_Champions_league_nodejs_express/src/server.ts
+++ 14_Champions_league_nodejs_express/src/server.ts
@@ -1,1 +1,9 @@
-console.log('hello world...');
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000);
