fd76fcc6e2499e0ef5317277c9432de83764cd29
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-17 11:56:04 -0300

    added - implementando cors

--- 14_Champions_league_nodejs_express/src/app.ts
+++ 14_Champions_league_nodejs_express/src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
 
 function createApp() {
   const app = express();
 
   app.use(express.json());
-
   app.use("/api", router);
+
+  const corsOptions = {
+    origin: ["http://brunao.itdallagnol.com", "http://gov.br"],
+    methods: ["GET", "UPDATE"],
+  };
+
+  app.use(cors());
 
   return app;
 }
