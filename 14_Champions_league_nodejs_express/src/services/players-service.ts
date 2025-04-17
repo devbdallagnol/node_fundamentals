c4d125403f2cdba9c02ca9773de075b44dc40416
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-17 11:20:16 -0300

    added - implementando delete service e repository

--- 14_Champions_league_nodejs_express/src/services/players-service.ts
+++ 14_Champions_league_nodejs_express/src/services/players-service.ts
@@ -3,55 +3,55 @@ import * as PlayerRepository from "../repositories/players-repository";
 import * as HttpResponse from "../utils/http-helper";
 
 export const getPlayerService = async () => {
-    const data = await PlayerRepository.findAllPlayers();
-    let response = null;
+  const data = await PlayerRepository.findAllPlayers();
+  let response = null;
 
-    if (data) {
-        response = await HttpResponse.ok(data);
-    }else {
-        response = await HttpResponse.noContent();
-    }
-    
-    return response;
-}
+  if (data) {
+    response = await HttpResponse.ok(data);
+  } else {
+    response = await HttpResponse.noContent();
+  }
+
+  return response;
+};
 
 export const getPlayerByIdService = async (id: number) => {
-    // pedir pro repositorio de dados
-    const data = await PlayerRepository.findPlayerById(id);
-    let response = null;
+  // pedir pro repositorio de dados
+  const data = await PlayerRepository.findPlayerById(id);
+  let response = null;
 
-    if (data) {
-        response = await HttpResponse.ok(data);
-    }else {
-        response = await HttpResponse.noContent();
-    }
+  if (data) {
+    response = await HttpResponse.ok(data);
+  } else {
+    response = await HttpResponse.noContent();
+  }
 
-    return response;
-}
+  return response;
+};
 
 export const createPlayerService = async (player: PlayerModel) => {
-    let response = null;
-  
-    //verifica se está vazio
-    if (Object.keys(player).length !== 0) {
-      await PlayerRepository.insertPlayer(player);
-      response = await HttpResponse.created();
-    } else {
-      response = await HttpResponse.badRequest();
-    }
-  
-    return response;
-  };
+  let response = null;
 
-  export const deletePlayerService = async (id: number) => {
-    let response = null;
-    const isDeleted = await PlayerRepository.deleteOnePlayer(id);
-  
-    if (isDeleted) {
-      response = await HttpResponse.ok({ message: "deleted" });
-    } else {
-      response = await HttpResponse.badRequest();
-    }
-  
-    return response;
-  };
+  //verifica se está vazio
+  if (Object.keys(player).length !== 0) {
+    await PlayerRepository.insertPlayer(player);
+    response = await HttpResponse.created();
+  } else {
+    response = await HttpResponse.badRequest();
+  }
+
+  return response;
+};
+
+export const deletePlayerService = async (id: number) => {
+  let response = null;
+  const isDeleted = await PlayerRepository.deleteOnePlayer(id);
+
+  if (isDeleted) {
+    response = await HttpResponse.ok({ message: "deleted" });
+  } else {
+    response = await HttpResponse.badRequest();
+  }
+
+  return response;
+};
